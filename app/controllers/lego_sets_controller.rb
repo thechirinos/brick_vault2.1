f11332460ab55f38a54ab9012fa7f252e47c2c2b
Prelang Builder <builder@prelang.com>
2016-01-08 02:51:43 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/lego_sets_controller.rb
+++ app/controllers/lego_sets_controller.rb
@@ -1,4 +1,8 @@
 class LegoSetsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_lego_set, only: [:show, :edit, :update, :destroy]
 
   # GET /lego_sets
