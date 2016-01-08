84e209d36662bedea631cec369d91cb6946084d6
Prelang Builder <builder@prelang.com>
2016-01-08 02:51:45 +0000

    Generated controller for root route with 'index' action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'set/index'
+
   resources :lego_sets
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
